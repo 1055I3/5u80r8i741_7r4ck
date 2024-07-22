e020c7e138be5d1f538941e0f3d12c58d2282d8c
milos milos <mduric@pm.me>
2024-07-22 23:24:17 +0200

    fixes

--- src/coordinates/cartesian_coordinates.jl
+++ src/coordinates/cartesian_coordinates.jl
@@ -1,15 +1,15 @@
-function cartesian_coordinates(a::Float64, e::Float64, i::Float64, omega::Float64, OMEGA::Float64, f::Float64)
+function cartesian_coordinates(a::Float64, e::Float64, i::Float64, ω::Float64, Ω::Float64, f::Float64)
     r::Float64 = (a - a*e^2) / (1 + e*cos(f))
-    sinOMEGA::Float64 = sin(OMEGA)
-    cosOMEGA::Float64 = cos(OMEGA)
-    sin_omega_f::Float64 = sin(omega + f)
-    cos_omega_f::Float64 = cos(omega + f)
+    sinΩ::Float64 = sin(Ω)
+    cosΩ::Float64 = cos(Ω)
+    sin_ω_f::Float64 = sin(ω + f)
+    cos_ω_f::Float64 = cos(ω + f)
     sin_i::Float64 = sin(i)
     cos_i::Float64 = cos(i)
 
-    X::Float64 = r * (cosOMEGA * cos_omega_f - sinOMEGA * sin_omega_f * cos_i)      # cartesian x coordinate
-    Y::Float64 = r * (sinOMEGA * cos_omega_f + cosOMEGA * sin_omega_f * cos_i)      # cartesian y coordinate
-    Z::Float64 = r * sin_omega_f * sin_i                                            # cartesian z coordinate
+    X::Float64 = r * (cosΩ * cos_ω_f - sinΩ * sin_ω_f * cos_i)      # cartesian x coordinate
+    Y::Float64 = r * (sinΩ * cos_ω_f + cosΩ * sin_ω_f * cos_i)      # cartesian y coordinate
+    Z::Float64 = r * sin_ω_f * sin_i                                            # cartesian z coordinate
 
     (X, Y, Z)
 end
