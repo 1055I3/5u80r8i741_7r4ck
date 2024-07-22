28f935d8aad292cb42d711a0ce821ea03ba23392
1055I3 <mduric@pm.me>
2024-07-22 22:05:41 +0200

    cleanup

--- main.jl
+++ main.jl
@@ -3,7 +3,7 @@ using src
 
 function main(ARGS::Vector{String})
     if length(ARGS) != 5
-        error("\nthe call should contain 4 arguments:\njulia main.jl [semi-major axis] [eccentricity] [inclination] [argument of periapsis] [revolutions]\n")
+        error("\nthe call should contain 5 arguments:\njulia main.jl [semi-major axis] [eccentricity] [inclination] [argument of periapsis] [revolutions]\n")
     end
 
     a::Float64 = parse(Float64, ARGS[1])                                            # semi-major axis [m]
@@ -18,16 +18,3 @@ function main(ARGS::Vector{String})
 end
 
 main(ARGS)
-
-# полупречник Земље - 6378km
-# молния a=26600e3, e=0.74, i=63.4, omega=-90
-# тундра a=42163e3, e=0.27, i=63.4, omega=-90
-# супертундра a=42163e3, e=0.423, i=63.4, omega=-90
-# лупус a=29991.5e3, e=0.6, i=63.4, omega=-90
-# вирго a=20260.2, e=0.66085, i=, omega=-90
-# вест a=20267.1, e=0, i=75, omega=0
-# гео a=42163e3, e=0, i=0, omega=0
-# нзо0 a=8000e3, e=0, i=30, omega=30
-# нзо1 a=6796e3, e=0.000126, i=51.638, omega=-100
-
-# julia -t "auto" ground_track.jl 29991.5e3 0.6 63.4 -90
